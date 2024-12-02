f5b215e7bfb13d336699a910d09356c3cc2c4b45
Mia <miaelanderson@gmail.com>
2024-12-02 09:23:44 -0600

    Initial Commit

---
+++ Coin Flip/Coin_FlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_FlipApp.swift
+//  Coin Flip
+//
+//  Created by Mia Anderson on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Coin_FlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
