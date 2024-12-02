58562e9b94486279d591f3836c1b3024a9d9823f
Mia <miaelanderson@gmail.com>
2024-12-02 10:00:28 -0600

    added dice mechanism

--- Coin Flip/ContentView.swift
+++ Coin Flip/ContentView.swift
@@ -8,17 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomValue = 0
+    @State private var rotation = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dice Roll!")
+                .font(.title).bold()
+            Image("head \(randomValue)")
+                .resizable()
+                .frame(width: 200, height: 200)
+                .padding()
+                .onTapGesture {
+                    chooseRandom(times: 4)
+                    withAnimation(.interpolatingSpring(Spring(stiffness: 10, damping: 2))) {
+                        rotation += 360
+                    }
+                }
+                .rotationEffect(.degrees(rotation))
+                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
         }
-        .padding()
+    }
+    func chooseRandom(times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue = Int.random(in: 1...4)
+                chooseRandom(times: times - 1)
+            }
+        }
     }
 }
-
 #Preview {
     ContentView()
 }
